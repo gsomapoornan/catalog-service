f762a64b2d6a16c8abe26c864ada8c519ff62f36
gsomapoornan <ganesh.poorna86@gmail.com>
2019-02-07 14:53:34 +0530

    Eureka & External config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into product(id,name,price) values(10,'Laptop',999);
-insert into product(id,name,price) values(20,'Mobile',888);
-insert into product(id,name,price) values(30,'Watch',777);
+insert into product(id,name,price) values(10,'Laptop',9991);
+insert into product(id,name,price) values(20,'Mobile',8881);
+insert into product(id,name,price) values(30,'Watch',7771);
